10c204c9ef11768fe48cc5f410f64bd89d3e2fd6
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-02-08 20:50:32 -0300

    chore: add error handler in database.js

--- infra/database.js
+++ infra/database.js
@@ -11,11 +11,15 @@ async function query(queryObject) {
 
   await client.connect();
 
-  const result = await client.query(queryObject);
+  try {
+    const result = await client.query(queryObject);
 
-  await client.end();
-
-  return result;
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
