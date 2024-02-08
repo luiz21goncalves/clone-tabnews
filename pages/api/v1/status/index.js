e033b3d9f775ca1f04a0b5464d627f80bf2b4cb2
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-02-08 20:51:57 -0300

    feat: add database info for endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,32 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 export default async function handle(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
+  const updateAt = new Date().toISOString();
 
-  console.log(result.rows);
-  return response.status(200).json({ key: "são acima da média" });
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;"
+  );
+  const databaseMaxConnections =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+  const databaseOpenedConnections =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  return response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnections),
+        opened_connections: databaseOpenedConnections,
+      },
+    },
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,17 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
 
-  expect(response.status).toBe(200);
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody).toEqual({
+    updated_at: parsedUpdatedAt,
+    dependencies: {
+      database: {
+        version: "16.0",
+        max_connections: 100,
+        opened_connections: 1,
+      },
+    },
+  });
 });
