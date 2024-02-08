eb86eba676889e4ecd1216e32934ad3bcdd8ceaf
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-02-07 21:51:50 -0300

    chore: add absolute import config

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,8 @@
-export default function handle(request, response) {
+import database from "infra/database.js";
+
+export default async function handle(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+
+  console.log(result.rows);
   return response.status(200).json({ key: "são acima da média" });
 }
