d3efb724b7409e95f8e1b05e3c01b19c6f1aa0ae
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-02-11 09:39:41 -0300

    chore: log postgres connection variables

--- infra/database.js
+++ infra/database.js
@@ -9,7 +9,13 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
+  console.log("Credencias do Postgres: ", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
     const result = await client.query(queryObject);
