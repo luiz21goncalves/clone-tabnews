de27369638d4ed8b779cd237fd92f67f4a745f07
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-03-20 21:19:25 -0300

    test: read environment variables and configure ESM

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,7 +1,20 @@
+import database from "infra/database";
+
+console.log("env", {
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
+
 test("GET /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   const responseBody = await response.json();
 
+  console.log(responseBody);
+
   expect(response.status).toBe(200);
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
