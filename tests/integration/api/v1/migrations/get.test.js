a2d64ad3dd7b2c5db35c176b65071158ecc6710a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-03-22 21:10:38 -0300

    chore: delete schema before all test for /api/v1/migrations

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,18 +1,14 @@
 import database from "infra/database";
 
-console.log("env", {
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-});
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+beforeAll(cleanDatabase);
 
 test("GET /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   const responseBody = await response.json();
-
-  console.log(responseBody);
 
   expect(response.status).toBe(200);
   expect(Array.isArray(responseBody)).toBe(true);
