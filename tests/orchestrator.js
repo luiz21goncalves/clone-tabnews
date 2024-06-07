fa26b0456d94185ac898ff951d2f3c1348e379b1
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-06-07 16:25:51 -0300

    refactor: make `npm test` more robust
    With async-retry dependency and orchestrator file

---
+++ tests/orchestrator.js
@@ -0,0 +1,24 @@
+import retry from "async-retry";
+
+async function waitForAllServices() {
+  async function waitForWebService() {
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+
+      if (response.status !== 200) {
+        throw new Error(`HTTP error ${response.status}`);
+      }
+    }
+
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+  }
+
+  await waitForWebService();
+}
+
+export default {
+  waitForAllServices,
+};
